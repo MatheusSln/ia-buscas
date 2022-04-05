dc4935d036d2854a0169c31147d4cd45f876c128
MatheusSln <matheustts8@gmail.com>
2022-04-05 20:21:33 -0300

    Implementando a busca não informada

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import espacoDeEstados.*;
-import estrategiasDeBusca.heuristica.*;
-
+import estrategiasDeBusca.cega.BuscaEmProfundidadeLimitada;
 public class Main {
 
 	@SuppressWarnings("rawtypes")
@@ -10,6 +9,8 @@ public class Main {
 		//char[] cfgIni = {'2','4','3','7','1','6','5',' ','8'};
 		char[] cfgIni = {'2','3',' ','7','4','1','5','8','6'};
 		//char[] cfgIni = {'7','2','3','4',' ','1','5','8','6'}; // OutOfMemory
+                char[] cfgFim = {'1','2','3','4','5','6','7','8',' '};
+                        
 
 		Puzzle8 puzzleInicial = new Puzzle8();
 		puzzleInicial.setEstado(cfgIni);
@@ -17,19 +18,30 @@ public class Main {
 		puzzleInicial.setAvaliacao( puzzleInicial.heuristica(Puzzle8.TABULEIRO_ORGANIZADO) );
 			
 		Puzzle8 puzzleFinal = new Puzzle8();
-		puzzleFinal.setEstado( Puzzle8.TABULEIRO_ORGANIZADO );
+		puzzleFinal.setEstado(cfgFim);
 		puzzleFinal.setCusto(0);
 		puzzleFinal.setAvaliacao(0);
-						
-		BuscaInformada busca = new AStar();
-		busca.setInicio(puzzleInicial);
-		busca.setObjetivo(puzzleFinal);
-		busca.buscar();
-		for(Estado e : busca.getCaminhoSolucao()) {
-			System.out.println(e);
+					
+                BuscaEmProfundidadeLimitada buscaPL = new BuscaEmProfundidadeLimitada(); //buscaPI = Busca de pronfundidade limitada
+
+		buscaPL.setInicio(puzzleInicial);
+		buscaPL.setObjetivo(puzzleFinal);
+		buscaPL.setLimite(0);
+
+		while (buscaPL.getCaminhoSolucao().isEmpty()) {
+			try{
+				buscaPL.buscar();
+			}catch (Exception e){
+				System.out.println("O limite nao é: " + buscaPL.getLimite());
+			}
+			buscaPL.setLimite(buscaPL.getLimite() + 1);
 		}
 
-		System.exit(0);
+		for(Estado e : buscaPL.getCaminhoSolucao()) {
+			System.out.println(e);
+			System.out.println(" ");
+		}
+                
+                System.exit(0);
 	}
-
 }
